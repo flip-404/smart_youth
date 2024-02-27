3cbf1f27ceb98d941924126428e76cd89ba1ecb7
KIMTAESEONG <aka404365@gmail.com>
2024-02-27 19:09:46 +0900

    feat: implemented create new workbook Header

--- src/components/create-workbook/new/Header.tsx
+++ src/components/create-workbook/new/Header.tsx
@@ -1,5 +1,19 @@
 export default function Header() {
-  // pt-[60px] px-[80px] gap-[80px] pb-[100px]
-
-  return <div className="w-full h-basic-section flex flex-col ">문제문제</div>;
+  return (
+    <div className="fixed h-[80px] z-[8] bg-white w-full px-[80px] flex gap-[20px] items-center">
+      <svg
+        width="36"
+        height="36"
+        viewBox="0 0 36 36"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M23.2628 6.99976L24.9185 8.65549L15.3115 18.2626L24.9185 27.8696L23.2628 29.5253L12 18.2626L23.2628 6.99976Z"
+          fill="black"
+        />
+      </svg>
+      <p>문제의 제목을 입력하세요.</p>
+    </div>
+  );
 }
