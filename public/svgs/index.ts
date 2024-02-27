d6b6d6d475edc54ba37a3c956fe0da67fd0d2fca
KIMTAESEONG <aka404365@gmail.com>
2024-02-27 15:53:39 +0900

    chore: export icon s

--- public/svgs/index.ts
+++ public/svgs/index.ts
@@ -2,5 +2,6 @@ import HomeIcon from "./home_icon.svg";
 import CreateIcon from "./create_icon.svg";
 import SolveIcon from "./solve_icon.svg";
 import MyIcon from "./my_icon.svg";
+import ProblemsIcon from "./problems_icon.svg";
 
-export { HomeIcon, CreateIcon, SolveIcon, MyIcon };
+export { HomeIcon, CreateIcon, SolveIcon, MyIcon, ProblemsIcon };
