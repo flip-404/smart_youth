dc3ed1b6b9bcdcf98ab7672aaa3b80f2cf486be7
KIMTAESEONG <aka404365@gmail.com>
2024-05-06 18:12:10 +0900

    feat: implement Header's SearchBar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 
+import Image from "next/image";
+
 export default function Header() {
   return (
     <div className="z-[10] fixed top-0 left-0 flex items-center justify-between w-full bg-white h-[3rem] px-[2rem] border-b border-[#C1C1C1]">
@@ -11,7 +13,40 @@ export default function Header() {
           디
         </span>
       </Link>
-      <div>프론트엔드</div>
+      <div className="flex gap-[0.5rem] items-center">
+        <div className="flex items-center bg-[#F2F4F7] rounded-[0.75rem] py-[1px] px-[12px] w-[11.25rem] h-[1.5rem] text-[0.75rem] text-[#98A2B3] font-[400] reading-[18px]">
+          ex. 2024 코딩테스트 문제
+        </div>
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g clip-path="url(#clip0_133_2328)">
+            <path
+              d="M10.5 18C14.6421 18 18 14.6421 18 10.5C18 6.35786 14.6421 3 10.5 3C6.35786 3 3 6.35786 3 10.5C3 14.6421 6.35786 18 10.5 18Z"
+              stroke="#667085"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M15.8037 15.8035L21.0003 21"
+              stroke="#667085"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </g>
+          <defs>
+            <clipPath id="clip0_133_2328">
+              <rect width="24" height="24" fill="white" />
+            </clipPath>
+          </defs>
+        </svg>
+      </div>
     </div>
   );
 }
