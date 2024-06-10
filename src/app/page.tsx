a4510020e3353bd4f5144dcbf4edfc15c8aaf622
KIMTAESEONG <aka404365@gmail.com>
2024-06-10 14:10:14 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import FirstSection from "@/components/home/FirstSection";
 import SecondSection from "@/components/home/SecondSection";
 import ThirdSection from "@/components/home/ThirdSection";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
+  const { data: session } = useSession();
+  console.log("session:", session);
+
   return (
     <div className="pl-[280px] ">
       <FirstSection />
