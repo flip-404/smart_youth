ce1e6e6d56b50d3cc0a9e1911f0ea1049439db1b
KIMTAESEONG <aka404365@gmail.com>
2024-06-10 14:12:41 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import FirstSection from "@/components/home/FirstSection";
 import SecondSection from "@/components/home/SecondSection";
 import ThirdSection from "@/components/home/ThirdSection";
