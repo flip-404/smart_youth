ae275122f5692ebdcc4f9c467ae749f442cd305f
KIMTAESEONG <aka404365@gmail.com>
2024-06-10 10:50:51 +0900

    test

---
+++ src/app/api/auth/authOptions.ts
@@ -0,0 +1,54 @@
+import type { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const authOptions: AuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "이메일",
+          type: "text",
+        },
+        password: { label: "비밀번호", type: "password" },
+      },
+
+      async authorize(credentials, req) {
+        const res = await fetch(`${process.env.NEXTAUTH_URL}/api/signIn`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: credentials?.email,
+            password: credentials?.password,
+          }),
+        });
+        const user = await res.json();
+
+        if (user) {
+          console.log("로그인 성공", user);
+          return user;
+        } else {
+          console.log("로그인 실패", user);
+          return null;
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/signIn",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+
+    async session({ session, token }) {
+      session.user = token as any;
+      return session;
+    },
+  },
+};
+
+export default authOptions;
